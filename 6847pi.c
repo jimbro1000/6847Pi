62ce9bea6f4afb774d1ea6ef6c9d9ef505f201eb
Julian Brown <julian@the-lair.info>
2022-02-18 12:55:53 +0000

    resolve misnumbered pins

--- 6847pi.c
+++ 6847pi.c
@@ -1,45 +1,64 @@
 #include "pico/stdlib.h"
 #include "6847pi.h"
 
+const uint32_t SLEEP = 5000;
+
+void reset_analog_output(const int pins[], int scale) {
+    for (int i = 0; i < scale; ++i) {
+        gpio_put(pins[i], 0);
+    }
+}
+
+void init_analog_output(const int pins[], int scale) {
+    for (int i = 0; i < scale; ++i) {
+        gpio_init(pins[i]);
+        gpio_set_dir(pins[i], GPIO_OUT);
+    }
+    reset_analog_output(pins, scale);
+}
+
+void analog_output(int value, const int pins[], int scale, int max, const int value_map[]) {
+    //reset_analog_output(pin0, scale);
+    if (value < max) {
+        int output = value_map[value];
+        for (int i = 0; i < scale; ++i) {
+            gpio_put(pins[i], output % 2);
+            output = output / 2;
+        }
+    }
+}
+
 int main() {
-    init_analog_output(Y_PIN0, Y_SCALE);
-    init_analog_output(B_PIN0, B_SCALE);
-    init_analog_output(A_PIN0, A_SCALE);
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    #ifdef ANALOG_6847_OUTPUT_H
+    init_analog_output(Y_PINS, Y_SCALE);
+    init_analog_output(B_PINS, B_SCALE);
+    init_analog_output(A_PINS, A_SCALE);
 
     uint a = A_MIN;
     uint b = B_MIN;
     uint y = Y_MIN;
     while (true) {
-        a = a++ % (A_MAX + 1);
-        b = b++ % (B_MAX + 1);
-        y = y++ % (Y_MAX + 1);
-        analog_output(a, A_PIN0, A_SCALE, A_MAP);
-        analog_output(b, B_PIN0, B_SCALE, B_MAP);
-        analog_output(y, Y_PIN0, Y_SCALE, Y_MAP);
+        reset_analog_output(A_PINS, A_SCALE);
+        reset_analog_output(B_PINS, B_SCALE);
+        reset_analog_output(Y_PINS, Y_SCALE);
+        a = (a + 1) % (A_MAX + 1);
+        b = (b + 1) % (B_MAX + 1);
+        y = (y + 1) % (Y_MAX + 1);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(SLEEP);
+        analog_output(a, A_PINS, A_SCALE, A_MAX, A_MAP);
+        analog_output(b, B_PINS, B_SCALE, B_MAX, B_MAP);
+        analog_output(y, Y_PINS, Y_SCALE, Y_MAX, Y_MAP);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(SLEEP);
     }
-}
-
-void init_analog_output(int pin0, int scale) {
-    for (int i = 0; i < scale; ++i) {
-        gpio_init(pin0 + i);
-        gpio_set_dir(pin0 + i, GPIO_OUT);
-    }
-    reset_analog_output(pin0, scale);
-}
-
-void reset_analog_output(int pin0, int scale) {
-    for (int i = 0; i < scale; ++i) {
-        gpio_put(pin0 + i, 1);
-    }
-}
-
-void analog_output(int value, int pin0, int scale, int[] value_map) {
-    reset_analog_output(pin0, scale);
-    if (value < len(value_map)) {
-        int output = value_map[value];
-        for (int i = 0; i < scale; ++i) {
-            gpio_put(pin0 + i, output % 2);
-            output = output / 2;
-        }
-    }
+    #else
+        gpio_put(LED_PIN, 0);
+        sleep_ms(300);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(300)
+    #endif
 }
--- 6847pi.h
+++ 6847pi.h
@@ -1,17 +1,17 @@
 #ifndef ANALOG_6847_OUTPUT_H
 #define ANALOG_6847_OUTPUT_H
 
-const int Y_PIN0 = 26;
+const int Y_PINS[3] = {31,32,34};
 const int Y_SCALE = 3;
 const int Y_MAX = 5;
 const int Y_MIN = 0;
 const int Y_MAP[6] = {0,1,3,4,6,7};
-const int B_PIN0 = 20;
+const int B_PINS[2] = {26,27};
 const int B_SCALE = 2;
 const int B_MAX = 2;
 const int B_MIN = 0;
 const int B_MAP[3] = {0,2,3};
-const int A_PIN0 = 18;
+const int A_PINS[2] = {24,25};
 const int A_SCALE = 2;
 const int A_MAX = 3;
 const int A_MIN = 0;
